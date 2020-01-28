a0875531f66cdfe2d7febc94b7c189767692daa2
Ian Kahrilas <ijk60068@gmail.com>
2020-01-27 16:25:32 -0800

    add YAML header

--- testing_script.R
+++ testing_script.R
@@ -1,3 +1,7 @@
+#' ---
+#' output: github_document
+#' ---
+
 # Load data -- fix this path so it works for you
 # dat <- read.csv("/Users/Kara/teaching-and-workshops/wtf-fix-paths/data/installed-packages.csv")
 
