a768090ca104d3d97e8657995a72d6ae10118a48
Ian Kahrilas <ijk60068@gmail.com>
2020-01-27 16:07:25 -0800

    Add R script, gitignore, and project

---
+++ testing_script.R
@@ -0,0 +1,8 @@
+# Load data -- fix this path so it works for you
+dat <- read.csv("/Users/Kara/teaching-and-workshops/wtf-fix-paths/data/installed-packages.csv")
+
+# fixed path
+dat <- read.csv(here::here("data", "installed-packages.csv"))
+
+# View data
+View(dat)
